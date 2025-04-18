b45fbede25b6e317282bfc74a5433979afba66f8
Husain8570 <164224195+Husain8570@users.noreply.github.com>
2025-04-18 22:48:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,11 @@ mongoose.connect(MONGODB_URI)
 const app=express();
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+  origin: "https://codetrack.onrender.com",
+  credentials: true
+}));
+
 app.use('/api/auth',userRoutes)
 app.use('/api/problems',problemRoutes)
 
