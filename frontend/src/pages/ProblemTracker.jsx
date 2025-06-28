4071b51a3fcd59dfc3ef9dd3012000bbe08be23d
Husain8570 <164224195+Husain8570@users.noreply.github.com>
2025-06-28 23:53:47 +0530

    Update ProblemTracker.jsx

--- frontend/src/pages/ProblemTracker.jsx
+++ frontend/src/pages/ProblemTracker.jsx
@@ -3,17 +3,11 @@ import Form from '../components/Form';
 import Problems from '../components/Problems';
 
 function ProblemTracker() {
-  const [refresh, setRefresh] = useState(false);
-
-  const handleProblemAdded = () => {
-    setRefresh(!refresh)
-  };
-  
 
   return (
     <div className="overflow-y-auto w-screen min-h-screen bg-gray-900 flex flex-col md:flex-row justify-center items-start gap-8 p-8">
-      <Form onProblemAdded={handleProblemAdded} />
-      <Problems refresh={refresh} setRefresh={setRefresh}/>
+      <Form/>
+      <Problems/>
     </div>
   );
 }
